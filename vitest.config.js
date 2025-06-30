f11d12490ef0548c90c81f2a37a76e881e2f2849
caroline sarre <anacarolinesarre@gmail.com>
2025-06-30 09:10:38 -0300

    adicionando arquivos de configuração de testes

---
+++ vitest.config.js
@@ -0,0 +1,12 @@
+/** 
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+
+export default {
+  plugins: [svelte()],
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./src/setupTests.js"
+  }
+};
+*/
